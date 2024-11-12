ff88ad900f1929372d72f7c9efe0eb1e4c8a0399
Kristaps <kbebris+git@gmail.com>
2024-11-12 20:36:42 +0200

    messing aroung

--- test.rb
+++ test.rb
@@ -1,96 +1,39 @@
 use_bpm 120
 
-define :silent_night_melody do
-  play :c4
-  sleep 0.75
-  play :c4
-  sleep 0.75
-  play :g4
-  sleep 0.75
-  play :g4
-  sleep 0.75
-  play :a4
-  sleep 0.75
-  play :a4
-  sleep 0.75
-  play :g4
-  sleep 1.5
-  
-  play :f4
-  sleep 0.75
-  play :f4
-  sleep 0.75
-  play :e4
-  sleep 0.75
-  play :e4
-  sleep 0.75
-  play :d4
-  sleep 0.75
-  play :d4
-  sleep 0.75
-  play :c4
-  sleep 1.5
-  
-  play :g4
-  sleep 0.75
-  play :g4
-  sleep 0.75
-  play :f4
-  sleep 0.75
-  play :f4
-  sleep 0.75
-  play :e4
-  sleep 0.75
-  play :e4
-  sleep 0.75
-  play :d4
-  sleep 1.5
-  
-  play :g4
-  sleep 0.75
-  play :g4
-  sleep 0.75
-  play :f4
-  sleep 0.75
-  play :f4
-  sleep 0.75
-  play :e4
-  sleep 0.75
-  play :e4
-  sleep 0.75
-  play :d4
-  sleep 1.5
-  
-  play :c4
-  sleep 0.75
-  play :c4
-  sleep 0.75
-  play :g4
-  sleep 0.75
-  play :g4
-  sleep 0.75
-  play :a4
-  sleep 0.75
-  play :a4
-  sleep 0.75
-  play :g4
-  sleep 1.5
-  
-  play :f4
-  sleep 0.75
-  play :f4
-  sleep 0.75
-  play :e4
-  sleep 0.75
-  play :e4
-  sleep 0.75
-  play :d4
-  sleep 0.75
-  play :d4
-  sleep 0.75
-  play :c4
+define :test do
+  play :G3, sustain: 2
   sleep 2
+  play :A3, sustain: 0.75
+  sleep 0.75
+  play :G3, sustain: 1.5
+  sleep 1.5
+  play :E3, sustain: 3
+  sleep 4
+  
+  play :G3, sustain: 2
+  sleep 2
+  play :A3, sustain: 0.75
+  sleep 0.75
+  play :G3, sustain: 1.5
+  sleep 1.5
+  play :E3, sustain: 3
+  sleep 4
+  
+  play :D4, sustain: 2.75
+  sleep 2.75
+  play :D4, sustain: 1.5
+  sleep 1.5
+  play :B3, sustain: 2.75
+  sleep 3.75
+  
+  play :C4, sustain: 2.75
+  sleep 2.75
+  play :C4, sustain: 1.5
+  sleep 1.5
+  play :G3, sustain: 3
+  sleep 3.75
+  
 end
 
-use_synth :bass_highend
-silent_night_melody
+use_synth :piano
+test
